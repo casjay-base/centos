becc7597e2404643c1895a17f21db5f3447be995
casjay <git-admin@casjaysdev.com>
2023-01-01 11:58:59 -0500

    🗃️ Committing everything that changed 🗃️

---
+++ root/.local/bin/update-resolv.sh
@@ -0,0 +1,36 @@
+#!/usr/bin/env bash
+# - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
+##@Version           :  202201210244-git
+# @@Author           :  Jason Hempstead
+# @@Contact          :  git-admin@casjaysdev.com
+# @@License          :  LICENSE.md
+# @@ReadME           :  update-resolv.sh --help
+# @@Copyright        :  Copyright: (c) 2022 Jason Hempstead, Casjays Developments
+# @@Created          :  Tuesday, Sep 06, 2022 15:18 EDT
+# @@File             :  update-resolv.sh
+# @@Description      :  Update the Resolver config
+# @@Changelog        :  newScript
+# @@TODO             :  Refactor code
+# @@Other            :
+# @@Resource         :
+# @@Terminal App     :  no
+# @@sudo/root        :  no
+# @@Template         :  bash/system
+# - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
+if [ -f "/etc/resolv.conf " ]; then
+  [ -n "$(builtin type -P chattr)" ] && chattr -i "/etc/resolv.conf"
+  mv -f "/etc/resolv.conf" "/tmp/resolv.bak.conf"
+fi
+# - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
+if curl -q -LSsf "https://github.com/casjay-base/centos/raw/main/etc/resolv.conf" -o "/tmp/resolv.conf"; then
+  [ -f "/tmp/resolv.conf" ] && mv -f "/tmp/resolv.conf" "/etc/resolv.conf"
+else
+  [ -f "/tmp/resolv.bak.conf" ] && mv -f "/etc/resolv.conf" "/tmp/resolv.bak.conf"
+fi
+# - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
+[ -f "/tmp/resolv.bak.conf" ] && rm -Rf "/tmp/resolv.bak.conf"
+[ -n "$(builtin type -P chattr)" ] && chattr +i "/etc/resolv.conf"
+# - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
+exit
+# - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
+# end
