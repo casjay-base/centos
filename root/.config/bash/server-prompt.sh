04a3a822980534571c9c2d2e57658e6b509c50a9
casjay <git-admin@casjaysdev.pro>
2023-08-23 18:02:37 -0400

    🗃️ Committing everything that changed 🗃️
    etc/skel/.bashrc
    etc/skel/.config/bash/server-prompt.sh
    root/.config/bash/server-prompt.sh

--- root/.config/bash/server-prompt.sh
+++ root/.config/bash/server-prompt.sh
@@ -1,22 +1,11 @@
+# Set ps2 and ps4
+PS2="⚡ " PS4="$(tput cr 2>/dev/null && tput cuf 6 2>/dev/null && printf "${GREEN}+%s ($LINENO) +" " $RESET")"
+
 # set title
 __ps1_set_title() { echo -ne "${USER}@${HOSTNAME}:${PWD//$HOME/\~}"; }
 
 # prompt prev exit status
-__ps1_promp_command() {
-  local retVal=$?
-  if [ $retVal = 0 ]; then
-    PS1="${RED}[\v]:${GREEN}[\u]@[\H]${RESET}:${YELLOW}[\w]${RESET}:${GREEN}[$retVal]${RESET}${BLACK}🐚${RESET}"
-  else
-    PS1="${RED}[\v]:${GREEN}[\u]@[\H]${RESET}:${YELLOW}[\w]${RESET}:${RED}[$retVal]${RESET}${BLACK}🐚${RESET}"
-  fi
-  return $retVal
-}
+__ps1_promp_command() { local retVal=$? && history -a && history -r && return $retVal; }
 
 # set default prompt
-PROMPT_COMMAND="__ps1_promp_command;__ps1_set_title;history -a && history -r"
-PS2="⚡ "
-PS4="$(
-  tput cr 2>/dev/null
-  tput cuf 6 2>/dev/null
-  printf "${GREEN}+%s ($LINENO) +" " $RESET"
-)"
+PROMPT_COMMAND="__ps1_set_title;__ps1_promp_command"
