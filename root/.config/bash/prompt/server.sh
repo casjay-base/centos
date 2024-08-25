3a4d0642ade4352541525dfc7db9a88e32ee14a2
casjay <git-admin@casjaysdev.pro>
2024-08-25 09:37:15 -0400

    🗃️ Committing everything that changed 🗃️
    etc/skel/.bashrc
    etc/skel/.config/bash/prompt/
    etc/skel/.config/bash/server-prompt.sh
    etc/skel/.config/pip/
    "root/.bashrc
    root/.bashrc
    root/.config/bash/prompt/server.sh
    root/.config/pip/

--- root/.config/bash/prompt/server.sh
+++ root/.config/bash/prompt/server.sh
@@ -1,5 +1,6 @@
 # Set ps2 and ps4
-PS2="⚡ " PS4="$(tput cr 2>/dev/null && tput cuf 6 2>/dev/null && printf "${GREEN}+%s ($LINENO) +" " $RESET")"
+PS2="⚡ "
+PS4="$(tput cr 2>/dev/null && tput cuf 6 2>/dev/null && printf "${GREEN}+%s ($LINENO) +" " $RESET")"
 
 # set title
 __ps1_set_title() { echo -ne "${USER}@${HOSTNAME}:${PWD//$HOME/\~}"; }
